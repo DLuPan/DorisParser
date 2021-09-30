013cf1dde0cd4274ef695a0f835b8d47289066b8
dinglupan <dinglupan@ky-tech.com.cn>
2021-09-30 16:09:46 +0800

    调整包名

--- src/main/java/com/lfsenior/sqlparser/doris/vistor/AbstractAdapterVistor.java
+++ src/main/java/com/lfsenior/sqlparser/doris/vistor/AbstractAdapterVistor.java
@@ -2,7 +2,7 @@ package com.lfsenior.sqlparser.doris.vistor;
 
 /**
  * @program: DorisParser
- * @package: com.kyexpress.dsp.metadata.provider.sqlparser.ddl.doris.vistor
+ * @package: com.lfsenior.sqlparser.doris.vistor
  * @description: 适配器
  * @author: DingLuPan
  * @create: 2021/9/29 15:06
--- src/main/java/com/lfsenior/sqlparser/doris/vistor/Vistor.java
+++ src/main/java/com/lfsenior/sqlparser/doris/vistor/Vistor.java
@@ -2,7 +2,7 @@ package com.lfsenior.sqlparser.doris.vistor;
 
 /**
  * @program: DorisParser
- * @package: com.kyexpress.dsp.metadata.provider.sqlparser.ddl.doris.vistor
+ * @package: com.lfsenior.sqlparser.doris.vistor
  * @description: 访问者模型
  * @author: DingLuPan
  * @create: 2021/9/29 15:06
